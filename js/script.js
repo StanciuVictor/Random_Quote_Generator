15e6a9c7fcf644c5ea8bd2bd3b14f73169cc7085
StanciuVictor <victor.stanciu@yahoo.com>
2021-09-22 14:18:36 +0300

    Corrected the code so that the quote changes when button is pressed
    Others:
    - implemented a randomNumber arrow function for DRY and neat code;
    - minor changes and typos in comments

--- js/script.js
+++ js/script.js
@@ -11,31 +11,42 @@ project 1 - A Random Quote Generator
 //Array containing 5 quote objects.
 const quotes = [
   {
-    quote: '"Believe you can and you\'re halfway there."',
+    quote: 'Believe you can and you\'re halfway there.',
     source: 'Theodore Roosevelt',       // the person or character who said it
     citation: 'Harvard',                // reference to the source of the quote (book, movie, song)
     year: '1905'                        // the year the quote originated.
   },
   {
-    quote: '"I would rather die of passion than of boredom."',
+    quote: 'I would rather die of passion than of boredom.',
     source: 'Vincent van Gogh',
     year: '1884'
   },
   {
-    quote: '"Eighty percent of success is showing up."',
+    quote: 'Eighty percent of success is showing up.',
     source: 'Woody Allen',
     year: '1982'
   },
   {
-    quote: '"You can never cross the ocean until you have the courage to lose sight of the shore."',
+    quote: 'You can never cross the ocean until you have the courage to lose sight of the shore.',
     source: 'Christopher Columbus'
   },
   {
-    quote: '"Go confidently in the direction of your dreams. Live the life you have imagined."',
+    quote: 'Go confidently in the direction of your dreams. Live the life you have imagined.',
     source: 'Henry David Thoreau'
   }
 ];
 // console.log(quotes);    // Test to see if the quotes array is OK.
+
+
+/**
+ * Returns a random number between 0 and a set number
+ * 
+ * @param {number} num - The set number
+ * @returns {number} The random number between 0 and set number
+ */
+
+let randomNumber = (num) => Math.floor(Math.random() * num);
+
 
 /**
 * Returns a random quote object from quotes array
@@ -44,34 +55,34 @@ const quotes = [
 * @returns {object} The quote object
 */
 function getRandomQuote(arr){
-    let index = Math.floor((Math.random() * (arr.length)));   // Get a random number between [0;5) => [0;4] (the last index of quotes array)
-  // console.log(`Index is: ${index}`);                       // Test
-    return arr[index];                                        // Return random quote object
+    let index = randomNumber(arr.length);          // Get a random number between [0;5) => [0;4] (the last index of quotes array)
+  // console.log(`Index is: ${index}`);            // Test
+    return arr[index];                             // Return random quote object
 }
 
+
 /**
- * Takes one random quote object from quotes array and generates html code
- * 
- * @returns {string} The html code needed to print the quote, source, citation and year.
+ * Takes one random quote object from quotes array and prints code in the quote-box
  */
 function printQuote(){
+  // Set random background color
+  document.body.style.backgroundColor = `rgb(${randomNumber(256)},${randomNumber(256)},${randomNumber(256)})`;
+  // Get random quote object
   let quoteObj = getRandomQuote(quotes);
   // console.log(quoteObj);       // Test
   let htmlText = `<p class="quote">${quoteObj.quote}</p><p class="source">${quoteObj.source}`;
-  // If there is a citation property with a value assigned to it, append it
+  // If there is a 'citation' property with a value assigned to it, append it
   if (typeof quoteObj.citation !== 'undefined') {
     htmlText += `<span class="citation">${quoteObj.citation}</span>`;
   }
-  // If there is a year property with a value assigned to it, append it
+  // If there is a 'year' property with a value assigned to it, append it
   if (typeof quoteObj.year !== 'undefined') {
     htmlText += `<span class="year">${quoteObj.year}</span>`;
   }
   htmlText += `</p>`;
-  return htmlText;
+  document.getElementById('quote-box').innerHTML = htmlText;
 }
 
-// Print the html code in the quote-box
-document.getElementById('quote-box').innerHTML = printQuote();
 
 /***
  * click event listener for the print quote button
