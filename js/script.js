9c6a912ead2eae836d0a763c73b7ee868b8da806
StanciuVictor <victor.stanciu@yahoo.com>
2021-09-22 11:33:55 +0300

    Added comments to the code

--- js/script.js
+++ js/script.js
@@ -7,9 +7,8 @@ project 1 - A Random Quote Generator
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
-***/
+
+//Array containing 5 quote objects.
 quotes = [
   {
     quote: '"Believe you can and you\'re halfway there."',
@@ -38,25 +37,32 @@ quotes = [
 ];
 // console.log(quotes);    // Test to see if the quotes array is OK.
 
-/***
- * `getRandomQuote` function
-***/
+/**
+* Returns a random quote object from quotes array
+*
+* @param {array} arr - The array from which this function takes one random object
+* @returns {object} The quote object
+*/
 function getRandomQuote(arr){
-  let index = Math.floor((Math.random() * (arr.length)));    // Get a random number between [0;5) => [0;4] (the last index of quotes array)
-  // console.log(`Index is: ${index}`);                      // Test
-  return arr[index];
+    let index = Math.floor((Math.random() * (arr.length)));   // Get a random number between [0;5) => [0;4] (the last index of quotes array)
+  // console.log(`Index is: ${index}`);                       // Test
+    return arr[index];                                        // Return random quote object
 }
 
-/***
- * `printQuote` function
-***/
+/**
+ * Takes one random quote object from quotes array and generates html code
+ * 
+ * @returns {string} The html code needed to print the quote, source, citation and year.
+ */
 function printQuote(){
   quoteObj = getRandomQuote(quotes);
+  // console.log(quoteObj);       // Test
   htmlText = `<p class="quote">${quoteObj.quote}</p><p class="source">${quoteObj.source}`;
-  // console.log(quoteObj);
+  // If there is a citation property with a value assigned to it, append it
   if (typeof quoteObj.citation !== 'undefined') {
     htmlText += `<span class="citation">${quoteObj.citation}</span>`;
   }
+  // If there is a year property with a value assigned to it, append it
   if (typeof quoteObj.year !== 'undefined') {
     htmlText += `<span class="year">${quoteObj.year}</span>`;
   }
@@ -64,6 +70,7 @@ function printQuote(){
   return htmlText;
 }
 
+// print the html code in the quote-box
 document.getElementById('quote-box').innerHTML = printQuote();
 
 /***
